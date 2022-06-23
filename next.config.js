7e5658a27c18b24cd01af00c70cd0fa89ad72781
Minung Han <hmu332233@gmail.com>
2022-06-24 02:52:13 +0900

    cors 허용 설정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const securityHeaders = [
-  {
-    key: 'X-Frame-Options',
-    value: 'allow-from http://localhost'
-  }
+  { key: "Access-Control-Allow-Credentials", value: "true" },
+  { key: "Access-Control-Allow-Origin", value: "*" },
+  { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
 ];
 
 /** @type {import('next').NextConfig} */
