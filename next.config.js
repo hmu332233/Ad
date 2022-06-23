17b2b6632ff5395d11808a97ab8db86101048620
Minung Han <hmu332233@gmail.com>
2022-06-24 02:43:24 +0900

    X-Frame-Options 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'allow-from http://localhost'
+  }
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
