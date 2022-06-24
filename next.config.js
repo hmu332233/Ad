f7bf81c8af9369c08e66490b43012fd5ce60de23
Minung Han <hmu332233@gmail.com>
2022-06-25 02:27:57 +0900

    header 수정

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
-const securityHeaders = [
-  { key: "Access-Control-Allow-Credentials", value: "true" },
-  { key: "Access-Control-Allow-Origin", value: "*" },
-  { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-];
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
     return [
       {
-        // Apply these headers to all routes in your application.
-        source: '/',
-        headers: securityHeaders,
-      },
+        "source": "/(.*)",
+        "headers": [
+          {
+            "key": "access-control-allow-origin",
+            "value": "*"
+          }
+        ]
+      }
     ]
   },
 }
