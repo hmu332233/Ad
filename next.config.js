199128c8c1a6ca8c8002a010883d9a06eb2e117f
Minung Han <hmu332233@gmail.com>
2022-06-25 02:49:12 +0900

    헤더 수정

--- next.config.js
+++ next.config.js
@@ -4,19 +4,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        "source": "https://t1.daumcdn.net/kas/static/ba.min.js",
-        "headers": [
-          {
-            "key": "access-control-allow-origin",
-            "value": "*"
-          },
-          {
-            "key": "cross-origin-resource-policy",
-            "value": "cross-origin"
-          }
-        ]
-      },
-      {
         "source": "/(.*)",
         "headers": [
           {
@@ -26,10 +13,6 @@ const nextConfig = {
           {
             "key": "cross-origin-resource-policy",
             "value": "cross-origin"
-          },
-          {
-            "key": "cross-origin-embedder-policy",
-            "value": "require-corp"
           }
         ]
       }
